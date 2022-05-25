ad0ebc6e94d48325ae0a6e5e985e37da64eab43e
Amira de Moya <demoyaamira@gmail.com>
2022-05-24 21:39:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+const { objDisplay } = require("chai/lib/chai/utils")
+
+const employee ={
+    name: '',
+    streetAddress: '',
+}
+
+function updateEmployeeWithAndValue(employee, key, value){
+    const newEmployee = {...employee}
+    newEmployee[key] = value
+    return newEmployee
+}
+
+
