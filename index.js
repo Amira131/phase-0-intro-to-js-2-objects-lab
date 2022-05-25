534cd189044f7afd1e6ebd3e5f8f54f4ca41dcd8
Amira de Moya <demoyaamira@gmail.com>
2022-05-25 10:03:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const { objDisplay } = require("chai/lib/chai/utils")
-
-const employee ={
+const employee = {
     name: '',
     streetAddress: '',
 }
 
-function updateEmployeeWithAndValue(employee, key, value){
-    const newEmployee = {...employee}
-    newEmployee[key] = value
-    return newEmployee
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return{...employee, [key]: value,
+    };
 }
 
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key, value){
+   const  newEmployee = {...employee};
+    newEmployee [key] = value;
+    
+    return newEmployee
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value){
+     employee[key]= value
+     return employee
+
+}
 
